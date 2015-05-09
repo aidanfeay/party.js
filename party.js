c8c3dc3c3f2e702799d9f24db2eb85f382e6d8eb
Aidan Feay <aidanlfeay@gmail.com>
2015-05-08 21:24:07 -0400

    formatting and comments

--- party.js
+++ party.js
@@ -3,25 +3,26 @@ var secretCode = function(code, action, time){
 	var timer;
 
 	$(document).keyup(function(e) {
-	   strokes += e.which;
-	   clearTimeout(timer);
-	   timer = setTimeout(function() {
-	   	keyStrokes();
-	   	strokes = "";
-	  }, time);
+		strokes += e.which;
+		clearTimeout(timer);
+		timer = setTimeout(function() {
+			keyStrokes();
+			strokes = "";
+		}, time);
 	});
 
 	function keyStrokes() {
-    if(strokes == code) {
-    	action();           
-    }
+		if(strokes == code) {
+			action();           
+		}
 	};
 };
 
 $(document).ready( function(){
+	// sample actions and calls
 	var keyParty = function() {
 		console.log("Party.js");
 	};
-	//secretCode requires a set of key codes, an action, and a reset period
+	// secretCode requires a set of key codes, an action, and a reset period
 	secretCode("8065828489",keyParty,1000);
 });
